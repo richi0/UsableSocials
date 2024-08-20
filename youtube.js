0caecb14a7c3b4018fcdac00c59799e182830c6b
richie <csspliskin@hotmail.com>
2024-08-20 16:36:52 +0200

    added blick and fixed youtube and linkedin

--- youtube.js
+++ youtube.js
@@ -1,4 +1,5 @@
 setInterval(() => {
+  // Youtube Section
   const preview = document.querySelectorAll(
     '[is-reel-item-style-avatar-circle =""]'
   );
@@ -6,17 +7,30 @@ setInterval(() => {
     el.style.display = "none";
   });
 
-  const elements = document.getElementsByTagName("span");
-  const suggested = Array.from(elements).filter(
-    (el) => el.innerHTML === "Suggested"
+  const shortCloseButtons = document.querySelectorAll(
+    '[aria-label="Not interested"]'
   );
-  suggested.forEach((el) => {
-    let parent = el.parentElement;
-    while (!parent.className.includes("relative")) {
-      parent = parent.parentElement;
+  Array.from(shortCloseButtons).forEach((el) => {
+    el.click();
+  });
+
+  // Linkedin Section
+  const posts = document.getElementsByClassName("update-components-header");
+  Array.from(posts).forEach((el) => {
+    const links = el.getElementsByTagName("a");
+    if (links.length == 0) {
+      el.parentElement.parentElement.style.display = "none";
     }
-    if (parent) {
-      parent.style.display = "none";
-    }
+  });
+
+  // Blick Section
+  document.body.className = "";
+  const modals = document.querySelectorAll(".tp-modal");
+  const backdrops = document.querySelectorAll(".tp-backdrop");
+  Array.from(modals).forEach((el) => {
+    el.style.display = "none";
+  });
+  Array.from(backdrops).forEach((el) => {
+    el.style.display = "none";
   });
 }, 1000);
